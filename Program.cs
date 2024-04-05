3b799ec78f38275bd2799ffce529076814b797e4
DepTraiHi <166094479+DepTraiHi@users.noreply.github.com>
2024-04-05 18:00:22 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,6 @@ namespace QLBH
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
         }
-        Console.wiriteline("Huuan");
+        Console.wiriteline("Huua..n");
     }
 }
